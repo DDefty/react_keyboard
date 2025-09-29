4580051bc2408471455ad448b74541a1e70d05bd
DDefty <dawid.kogut99@gmail.com>
2025-09-29 13:22:26 +0200

    Refactor App component to manage key press state and display last pressed key

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    lastClicked: null,
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ lastClicked: event.key });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.lastClicked === null ? (
+          <p className="App__message">Nothing was pressed yet</p>
+        ) : (
+          <p className="App__message">
+            The last pressed key is [{this.state.lastClicked}]
+          </p>
+        )}
+      </div>
+    );
+  }
+}
